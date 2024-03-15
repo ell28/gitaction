70537517b98c4f1b37d21de4810b57f4443905eb
ell28 <if-22043@students.ithb.ac.id>
2024-03-15 17:36:06 +0700

    Added func to count

--- main.go
+++ main.go
@@ -6,4 +6,14 @@ func main() {
 	fmt.Println("Hello, world!")
 	fmt.Println("Hello, world2!")
 	fmt.Println("Test Commit 3.")
+	fmt.Println("Test Commit 4.")
+	countNumbers()
+}
+
+func countNumbers() {
+	count := 0
+	for i := 0; i <= 10; i++ {
+		count++
+	}
+	fmt.Println("Total numbers:", count)
 }
